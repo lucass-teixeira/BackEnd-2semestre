d6f4432874cd4519aba7f954c2598c577455881b
LucasFelicianoATeixeira <lucasfeliciano326@gmail.com>
2020-02-14 10:09:55 -0300

    14/02/2020

---
+++ GeneroRepository.cs
@@ -0,0 +1,49 @@
+﻿using senai.filmes.webapi.Domains;
+using senai.filmes.webapi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace senai.filmes.webapi.Repositories
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        private string StringConexao = "Data Source=DEV18\\SQLEXPRESS; initial catalog=Filmes; user Id=sa; pwd=sa@132";
+        public List<GeneroDomain> Listar()
+        {
+            List<GeneroDomain> generos = new List<GeneroDomain>();
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string query = "SELECT IdGenero, Nome FROM Genero";
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        GeneroDomain genero = new GeneroDomain
+                        {
+                            IdGenero = Convert.ToInt32(rdr[0]),
+                            Nome = rdr["Nome"].ToString()
+                        };
+
+                        generos.Add(genero);
+
+
+                    }
+                }
+
+            }
+            return generos;
+        }
+    }
+}
+
